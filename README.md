a46a627f9e6a362c717a8673ece9949e69f01048
ThisIsSoSteve <steve.caldwell22@gmail.com>
2023-04-01 15:51:37 +0100

    added debug options for the star background added more detail to the readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 My portfolio project using three js (very much work in progress)
 
 ## How to run
-Install node LTS https://nodejs.org/en/
+Install Node.js LTS https://nodejs.org/en/
 
 Run the following in the directory
 
@@ -13,10 +13,13 @@ npm run dev
 ```
 
 ## Using
-- Vite (build)
-- React Three Fiber (three.js for react)
-- React Three Drei (helper for Fiber)
-- Tailwindcss - (styles)
+- Vite (Build) 
+- React Three Fiber (Three.js for React) - https://github.com/pmndrs/react-three-fiber
+- React Three Drei (Helpers for Fiber) - https://github.com/pmndrs/drei
+- Tailwindcss (Styles) - https://tailwindcss.com/
+- Leva (Debug menu) - https://github.com/pmndrs/leva
+- r3f-perf (monitor performace) - https://github.com/utsuboco/r3f-perf
 
 
-
+## Referances 
+- Leva Storybook https://leva.pmnd.rs/?path=/story/misc-input-options--optional
--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,22 +1,81 @@
 import { Stars } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import { folder, useControls } from "leva";
 import { useRef } from "react";
 
 export default function Background() {
 
+    const background = useControls({
+        'Star Background': folder({
+            rotationSpeed: {
+                value: { x: 0.02, y: 0.01 },
+                step: 0.01,
+                max: 1,
+                min: -1,
+                label: 'Rotation Speed',
+                hint: 'The speed of star rotation'
+            },
+            radius: {
+                value: 100,
+                step: 1,
+                min: 0,
+                label: 'Radius',
+            },
+            depth: {
+                value: 50,
+                step: 1,
+                min: 0
+                ,
+                label: 'Depth',
+            },
+            count: {
+                value: 5000,
+                step: 100,
+                min: 0,
+                label: 'Count',
+                hint: 'How many stars there are'
+            },
+            factor: {
+                value: 4,
+                step: 1,
+                min: 0,
+                label: 'Factor',
+            },
+            saturation: {
+                value: 0,
+                step: 1,
+                min: 0,
+                label: 'Saturation',
+            },
+            fade: {
+                value: true,
+                label: 'Fade',
+            },
+            speed: {
+                value: 1,
+                step: 1,
+                min: 0,
+                label: 'Speed',
+                hint: 'The speed how fast the stars twinkle',
+                //disabled: true
+            },
+        },
+            { collapsed: true }
+        )
+    })
     const group = useRef()
 
     useFrame((state, delta,) => {
+        group.current.rotation.x += background.rotationSpeed.y * delta
+        group.current.rotation.y += background.rotationSpeed.x * delta
 
-        group.current.rotation.y += 0.02 * delta
-        group.current.rotation.x += 0.01 * delta
     });
 
 
 
     return (
         <group ref={group}>
-            <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+            <Stars {...background} />
         </group>
     )
 }
